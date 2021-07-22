99ae3e58aadac1f4e6253f1e4e82d6246747e777
jhonboy121 <alfredmathew05@gmail.com>
2021-07-22 17:38:18 +0530

    sm8150-common: update camera hal packages
    as per the changes in device/oneplus/common
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- common.mk
+++ common.mk
@@ -154,7 +154,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:64 \
     android.hardware.camera.provider@2.4-service_64 \
     libxml2 \
-    vendor.oneplus.camera.CameraHIDL@1.0.vendor \
+    vendor.oneplus.hardware.camera@1.0.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # Common init scripts
