377fc70028506561fb2ed77888d286c4727c625c
jhonboy121 <alfredmathew05@gmail.com>
2021-08-04 14:09:52 +0530

    sm8150-common: use correct path for qcom sepolicy
    part of unfucking the sauce
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -148,7 +148,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2021-03-01
 
 # SELinux
-include device/qcom/sepolicy-vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/public
