4194dca3d120affaa276ac1f0f8980ddda4ebb22
TheScarastic <warabhishek@gmail.com>
2021-10-18 08:05:24 +0200

    sm8150-common: build 2.3 fingerprint HIDL
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- common.mk
+++ common.mk
@@ -244,8 +244,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.oneplus \
-    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.oneplus \
+    android.hardware.biometrics.fingerprint@2.3-service.oneplus \
     vendor.oneplus.hardware.display@1.0.vendor
 
 # GPS
