0b103ba04d109ef548d93753ceb7892bd406c54d
Pranav <npv12@iitbbs.ac.in>
2021-12-17 11:26:12 +0530

    sm8150-common: Move nfc conf from vendor to dt
    * We were not going to update them anyway. Lets just move it here instead of pinning them
    
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- common.mk
+++ common.mk
@@ -323,6 +323,10 @@ PRODUCT_PACKAGES += \
     Tag \
     android.hardware.nfc@1.2-service
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf 
+
 # OMX
 PRODUCT_PACKAGES += \
     libavservices_minijail \
