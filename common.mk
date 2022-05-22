65896aa67d08a4f83447ccb19143f3d2583cc296
Danny Baumann <dannybaumann@web.de>
2022-05-22 17:34:42 +0900

    sm8150-common: Add missing USB properties
    The missing diag property actually broke the ability to use diag mode.
    
    Change-Id: I135dc2f739e969a30a269652f599f4a79bbafceb
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- common.mk
+++ common.mk
@@ -360,6 +360,11 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2 
 
+# USB
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.usb.diag.func.name=diag \
+    vendor.usb.use_ffs_mtp=0
+
 # Vibrator
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service.oneplus
